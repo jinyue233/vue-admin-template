c34b26ab90965b2e237da9bcfe41965b9dc13d22
yuanmabiji <13570990660@163.com>
2022-02-05 17:53:58 +0800

    活动管理修改，删除和代码优化

--- src/api/gathering.js
+++ src/api/gathering.js
@@ -1,24 +1,56 @@
 import request from '@/utils/request'
+const api_name='gathering'
 export default {
     getList() {
         return request({
-        url: '/gathering',
-        method: 'get'
+            url: '/${api_name}',
+            method: 'get'
         })
     },
 
     search(page,size) {
         return request({
-        url: `/gathering/search/${page}/${size}`,
-        method: 'post'
+            url: `/${api_name}/search/${page}/${size}`,
+            method: 'post'
         })
-    }
-    ,
+    },
+
     search(page,size,searchMap) {
         return request({
-            url: `/gathering/search/${page}/${size}`,
+            url: `/${api_name}/search/${page}/${size}`,
             method: 'post',
             data: searchMap
         })
+    },
+
+    save(pojo) {
+        return request({
+            url: `/${api_name}`,
+            method: 'post',
+            data: pojo
+        })
+    },
+
+    findById(id) {
+        return request({
+            url: `/${api_name}/${id}`,
+            method: 'get'
+        })
+    },
+
+    update(id,pojo) {
+        return request({
+          url: `/${api_name}/${id}`,
+          method: 'put',
+          data: pojo
+        })
+    },
+
+    deleteById(id){
+        return request({
+          url: `/${api_name}/${id}`,
+          method: 'delete'
+        })
     }
+
 }
--- src/views/table/gathering.vue
+++ src/views/table/gathering.vue
@@ -14,6 +14,7 @@
                 <el-date-picker  type="date" placeholder="选择截止日期" v-model="searchMap.starttime_2" ></el-date-picker>
                 </el-form-item>
                 <el-button type="primary" @click="fetchData()">查询</el-button>
+                <el-button type="primary" @click="dialogFormVisible=true">新增</el-button>
             </el-form>
             <!--表格-->
             <el-table
@@ -45,6 +46,10 @@
                 label="截止日期">
             </el-table-column>
             <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button type="warning" plain size="small" @click="handleEdit(scope.row.id)" >修改</el-button>
+                    <el-button type="warning" plain size="small" @click="handleDelete(scope.row.id)" >删除</el-button>
+                </template>
             </el-table-column>
             </el-table>
 
@@ -55,6 +60,47 @@
             layout="prev, pager, next, jumper"
             :total="total">
           </el-pagination>
+
+          <!--编辑窗口-->
+        <el-dialog title="编辑" :visible.sync="dialogFormVisible" >
+          <el-form ref="form" :model="pojo" label-width="80px">
+            <el-form-item label="活动名称">
+              <el-input v-model="pojo.name"></el-input>
+            </el-form-item>
+            <el-form-item label="基本地点">
+              <el-input v-model="pojo.address"></el-input>
+            </el-form-item>
+            <el-form-item label="开始日期">
+              <el-date-picker type="date" placeholder="选择日期" v-model="pojo.starttime" style="width: 100%;" ></el-date-picker>
+            </el-form-item>
+            <el-form-item label="截止日期">
+              <el-date-picker type="date" placeholder="选择日期" v-model="pojo.endtime" style="width: 100%;" ></el-date-picker>
+            </el-form-item>
+            <el-form-item label="报名截止">
+              <el-date-picker type="date" placeholder="选择日期" v-model="pojo.enrolltime" style="width: 100%;" ></el-date-picker>
+            </el-form-item>
+            <el-form-item label="活动详情">
+              <el-input v-model="pojo.detail" type="textarea"> </el-input>
+            </el-form-item>
+            <el-form-item label="城市">
+              <el-select v-model="pojo.city" placeholder="请选择">
+                <el-option
+                  v-for="item in cityList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id">
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="是否可见" prop="delivery">
+              <el-switch on-text="" off-text=""  active-value="1" inactive-value="0" v-model="pojo.state" ></el-switch>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="handleSave" >修改</el-button>
+              <el-button @click="dialogFormVisible = false">取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
         </div>
       </el-col>
     </el-row>
@@ -63,6 +109,8 @@
 
 <script>
   import gathering from '@/api/gathering'
+  import city from '@/api/city'
+  import message from '@/utils/message'
 
   export default {
     data() {
@@ -71,22 +119,78 @@
         listLoading: true,
         total: 0,
         currentPage: 1,
-        searchMap: {}
+        searchMap: {},
+        pojo: {},
+        dialogFormVisible: false,
+        cityList: [],
+        id: ''
       }
     },
     created() {
       this.fetchData()
+      city.getList().then(response =>{
+        this.cityList = response.data
+      })
     },
     methods: {
       fetchData() {
         this.listLoading = true
-        gathering.search(this.currentPage,10,this.searchMap).then(response => {
-          if(response.flag === true){
+        gathering.search(this.currentPage, 10, this.searchMap).then(response => {
+          if (response.flag === true) {
             this.list = response.data.rows
             this.total = response.data.total
           }
           this.listLoading = false
         })
+      },
+
+      handleEdit(id) {
+        this.dialogFormVisible = true
+        this.id = id
+        if(this.id != ""){
+          gathering.findById(this.id).then( response => {
+            if (response.flag === true) {
+              this.pojo = response.data
+            }
+          })
+        }else{
+          this.pojo ={} // 清空表单
+        }
+      },
+
+      handleSave() {
+        if(this.id != ''){// 修改
+          message.handleShowMessage(gathering.update(this.id, this.pojo), this)
+        }else{
+          this.pojo ={} // 清空表单
+          message.handleShowMessage(gathering.save(this.id, this.pojo), this) 
+        }
+        this.dialogFormVisible = false // 隐藏窗口
+      },
+
+      handleDelete(id) {
+        this.$confirm('确定要删除吗？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          message.handleShowMessage(gathering.deleteById(id), this)
+        });
+      },
+
+      handleShowMessage(response) {
+        if (response.flag === true) {
+          this.$message({
+            message: response.message,
+            type: 'success'
+          });
+          this.fetchData();// 重新刷新列表
+        } else {
+          this.$message({
+            message: response.message,
+            type: 'error'
+          });
+        }
       }
     }
   }
